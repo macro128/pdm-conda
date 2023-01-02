d16ad7ad8ff25e68b908ee33b4d1cd19da0500b0
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-02 20:24:42 -0300

    plugin config

---
+++ pdm_conda/models/config.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass, field
+
+from pdm.exceptions import ProjectError
+from pdm.project import Project
+
+
+@dataclass
+class PluginConfig:
+    channels: list[str] = field(default_factory=lambda: [])
+    runner: str = "conda"
+    dependencies: list[str] = field(default_factory=lambda: [], repr=False)
+    optional_dependencies: dict[str, list] = field(default_factory=lambda: dict())
+    dev_dependencies: dict[str, list] = field(default_factory=lambda: dict())
+
+    def __post_init__(self):
+        if not self.channels:
+            self.channels = ["defaults"]
+        if self.runner not in ["conda", "micromamba", "mamba"]:
+            raise ProjectError(f"Invalid Conda runner: {self.runner}")
+
+    @classmethod
+    def load_config(cls, project: Project, **kwargs) -> "PluginConfig":
+        config = {
+            k.replace("-", "_"): v
+            for k, v in project.pyproject.settings.get("conda", {}).items()
+        }
+        return PluginConfig(**(config | kwargs))
+
+    def command(self, cmd=None):
+        cmd = cmd or "install"
+        _command = [self.runner, cmd, "-y"]
+        if cmd in ("install", "create"):
+            _command.append("--strict-channel-priority")
+        return _command
