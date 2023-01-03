13719711912b38c043114732569c18b7bac23b27
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-02 21:01:32 -0300

    move files to match with pdm

--- pdm_conda/__init__.py
+++ pdm_conda/__init__.py
@@ -4,9 +4,10 @@ from pdm.core import Core
 def main(core: Core):
     from pdm.signals import pre_lock
 
-    from pdm_conda import project, providers, repositories
-    from pdm_conda.models import candidates, requirements
+    from pdm_conda import project
+    from pdm_conda.models import candidates, repositories, requirements
     from pdm_conda.plugin import lock_conda_dependencies
+    from pdm_conda.resolver import providers
 
     pre_lock.connect(lock_conda_dependencies)
 
