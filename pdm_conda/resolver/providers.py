24e9b37f6ddf0e5b8dd71af6f7175f33c126a39c
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-05 16:31:50 -0300

    better patch

--- pdm_conda/resolver/providers.py
+++ pdm_conda/resolver/providers.py
@@ -1,10 +1,13 @@
 import functools
 from typing import Iterator, Mapping
 
+from pdm.models.candidates import Candidate
 from pdm.resolver.providers import BaseProvider
 
 from pdm_conda.models.candidates import CondaCandidate
 from pdm_conda.models.requirements import CondaRequirement, Requirement
+
+_patched = False
 
 
 def wrap_find_matches(func):
@@ -13,26 +16,16 @@ def wrap_find_matches(func):
         self,
         identifier: str,
         requirements: Mapping[str, Iterator[Requirement]],
-        *args,
-        **kwargs
+        incompatibilities: Mapping[str, Iterator[Candidate]],
     ):
         req = next(requirements.get(identifier, []), None)  # type: ignore
         if isinstance(req, CondaRequirement):
-            return [
-                CondaCandidate(
-                    req,
-                    name=req.name,
-                    version=list(req.specifier)[0].version if req.specifier else None,
-                    link=req.link,
-                ),
-            ]
-        return func(
-            self, identifier=identifier, requirements=requirements, *args, **kwargs
-        )
+            return lambda: [CondaCandidate.from_conda_requirement(req)]
+        return func(self, identifier, requirements, incompatibilities)
 
     return wrapper
 
 
-if not hasattr(BaseProvider, "_patched"):
-    setattr(BaseProvider, "_patched", True)
-    BaseProvider.find_matches = wrap_find_matches(BaseProvider.find_matches)
+if not _patched:
+    setattr(BaseProvider, "find_matches", wrap_find_matches(BaseProvider.find_matches))
+    _patched = True
