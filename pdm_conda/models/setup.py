b8fe754f00c10dca6d44981f5d820fb298a03f85
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-11 22:46:28 -0300

    add extras to conda setup distribution

--- pdm_conda/models/setup.py
+++ pdm_conda/models/setup.py
@@ -1,9 +1,13 @@
 from typing import Any
 
-from pdm.models.setup import SetupDistribution
+from pdm.models.setup import Setup, SetupDistribution
 
 
 class CondaSetupDistribution(SetupDistribution):
+    def __init__(self, data: Setup, extras: dict | None = None) -> None:
+        self.extras = extras or dict()
+        super().__init__(data)
+
     @property
     def metadata(self) -> dict[str, Any]:
         metadata = super().metadata
