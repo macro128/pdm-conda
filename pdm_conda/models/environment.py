7127c9a6dd49a4e52624546d14eaef2df36762ed
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-16 23:33:33 -0300

    convert working set python deps names to conda ones

--- pdm_conda/models/environment.py
+++ pdm_conda/models/environment.py
@@ -4,8 +4,10 @@ from pdm.models.environment import Environment, GlobalEnvironment
 from pdm.models.working_set import WorkingSet
 from pdm.project import Project
 
+from pdm_conda.mapping import pypi_to_conda
 from pdm_conda.plugin import conda_list
 from pdm_conda.project import CondaProject
+from pdm_conda.utils import normalize_name
 
 
 class CondaEnvironment(Environment):
@@ -15,7 +17,9 @@ class CondaEnvironment(Environment):
 
     def get_working_set(self) -> WorkingSet:
         working_set = super().get_working_set()
-        working_set._dist_map.update(conda_list(self.project))
+        working_set._dist_map = conda_list(self.project) | {
+            normalize_name(pypi_to_conda(dist.metadata["Name"])): dist for dist in working_set._dist_map.values()
+        }
         return working_set
 
 
