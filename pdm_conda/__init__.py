9898c3321d1a5afba4bff1a3c9282cd11c7b4a2d
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-16 23:15:00 -0300

    PluginConfig changes update pyproject.settings

--- pdm_conda/__init__.py
+++ pdm_conda/__init__.py
@@ -7,7 +7,7 @@ def main(core: Core):
     from pdm_conda import utils
     from pdm_conda.cli.commands.add import Command as AddCommand
     from pdm_conda.cli.commands.remove import Command as RemoveCommand
-    from pdm_conda.models.config import PluginConfig
+    from pdm_conda.models.config import CONFIGS
     from pdm_conda.plugin import lock_conda_dependencies
     from pdm_conda.project import CondaProject
 
@@ -17,7 +17,7 @@ def main(core: Core):
     core.register_command(RemoveCommand)
 
     pre_lock.connect(lock_conda_dependencies)
-    for name, config in PluginConfig.configs():
+    for name, config in CONFIGS:
         core.add_config(name, config)
 
 
--- pdm_conda/models/config.py
+++ pdm_conda/models/config.py
@@ -1,26 +1,117 @@
+import os
+from contextlib import contextmanager
 from dataclasses import dataclass, field
+from functools import wraps
 from pathlib import Path
+from typing import Any
 
 from pdm.exceptions import ProjectError
 from pdm.project import ConfigItem, Project
 
+from pdm_conda.mapping import DOWNLOAD_DIR_ENV_VAR
+
+_CONFIG_MAP = {"mapping_download_dir": "pypi-mapping.download-dir"}
+CONFIGS = [
+    ("runner", ConfigItem("Conda runner executable", "conda")),
+    ("channels", ConfigItem("Conda channels to use")),
+    ("as-default-manager", ConfigItem("Use Conda to install all possible requirements", False)),
+    ("dependencies", ConfigItem("Dependencies to install with Conda")),
+    ("optional-dependencies", ConfigItem("Optional dependencies to install with Conda")),
+    ("dev-dependencies", ConfigItem("Development dependencies to install with Conda")),
+    (
+        "pypi-mapping.download-dir",
+        ConfigItem(
+            "PyPI-Conda mapping download directory",
+            Path().home() / ".pdm-conda/",
+            env_var=DOWNLOAD_DIR_ENV_VAR,
+        ),
+    ),
+]
+CONFIGS = [(f"conda.{name}", config) for name, config in CONFIGS]
+
+
+def is_decorated(func):
+    return hasattr(func, "__wrapped__")
+
+
+def is_conda_config_initialized(project: Project):
+    return "conda" in project.pyproject.settings
+
 
 @dataclass
 class PluginConfig:
-    mappings_download_dir: Path
+    _project: Project = field(repr=False, default=None)
+    _initialized: bool = field(repr=False, default=False, compare=False)
+    _set_project_config: bool = field(repr=False, default=False, compare=False)
+
     channels: list[str] = field(default_factory=list)
     runner: str = "conda"
     as_default_manager: bool = False
     dependencies: list[str] = field(default_factory=list, repr=False)
     optional_dependencies: dict[str, list] = field(default_factory=dict)
     dev_dependencies: dict[str, list] = field(default_factory=dict)
-    _initialized: bool = field(repr=False, default=False)
+    mapping_download_dir: Path = field(repr=False, default=Path())
 
     def __post_init__(self):
-        if not self.channels:
-            self.channels = ["defaults"]
-        if self.runner not in ["conda", "micromamba", "mamba"]:
-            raise ProjectError(f"Invalid Conda runner: {self.runner}")
+
+        with self.omit_set_project_config():
+            if not self.channels:
+                self.channels = ["defaults"]
+            if self.runner not in ["conda", "micromamba", "mamba"]:
+                raise ProjectError(f"Invalid Conda runner: {self.runner}")
+
+        to_suscribe = self._project.pyproject._data
+        if not is_decorated(to_suscribe.update):
+            to_suscribe.update = self.suscribe(self, to_suscribe.update)
+        self._set_project_config = True
+
+    def __setattr__(self, name: str, value: Any) -> None:
+        super().__setattr__(name, value)
+        # if plugin config is set then maybe update pyproject settings
+        if not name.startswith("_") and not callable(getattr(self, name)):
+            name = f"conda.{_CONFIG_MAP.get(name, name)}".replace("_", "-")
+            name, config_item = next(filter(lambda n: name == n[0], CONFIGS))
+            if self._set_project_config:
+                name_path = name.split(".")
+                name = name_path.pop(-1)
+                config = self._project.pyproject.settings
+                for p in name_path:
+                    config = config.setdefault(p, dict())
+                config[name] = value
+                self._initialized = True
+            if config_item.env_var:
+                os.environ.setdefault(config_item.env_var, str(value))
+
+    @staticmethod
+    def suscribe(config, func):
+        """
+        Suscribe to function call and after executed refresh config
+        :param config: PluginConfig to refresh
+        :param func: function to decorate
+        """
+
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            _conf = config.load_config(config._project)
+            with config.omit_set_project_config():
+                for k, v in _conf.__dict__.items():
+                    if not callable(v) and k not in ("_project", "_set_project_config") and getattr(config, k) != v:
+                        setattr(config, k, v)
+            return result
+
+        return wrapper
+
+    @contextmanager
+    def omit_set_project_config(self):
+        """
+        Context manager that deactivates updating pyproject settings
+        :return:
+        """
+        old_value = self._set_project_config
+        self._set_project_config = False
+        yield
+        self._set_project_config = old_value
 
     @property
     def is_initialized(self):
@@ -35,10 +126,10 @@ class PluginConfig:
         :return: plugin configs
         """
         config = {k.replace("-", "_"): v for k, v in project.pyproject.settings.get("conda", {}).items()}
-        mapping_config = config.pop("pypi_mapping", dict())
-        config["mappings_download_dir"] = Path(mapping_config.get("download-dir", None) or Path.home() / ".pdm-conda/")
-        kwargs["_initialized"] = "conda" in project.pyproject.settings
-        return PluginConfig(**(config | kwargs))
+        kwargs["_initialized"] = is_conda_config_initialized(project)
+        name = "mapping_download_dir"
+        config[name] = Path(project.config[f"conda.{_CONFIG_MAP.get(name, name)}"])
+        return PluginConfig(_project=project, **(config | kwargs))
 
     def command(self, cmd="install"):
         """
@@ -50,16 +141,3 @@ class PluginConfig:
         if cmd in ("install", "create", "search"):
             _command.append("--strict-channel-priority")
         return _command
-
-    @classmethod
-    def configs(cls):
-        _configs = [
-            ("runner", ConfigItem("Conda runner executable", "conda")),
-            ("channels", ConfigItem("Conda channels to use", ["defaults"])),
-            ("as_default_manager", ConfigItem("Use Conda to install all possible requirements", False)),
-            ("dependencies", ConfigItem("Dependencies to install with Conda", [])),
-            ("optional-dependencies", ConfigItem("Optional dependencies to install with Conda", dict())),
-            ("dev-dependencies", ConfigItem("Development dependencies to install with Conda", dict())),
-            ("pypi-mapping.download-dir", ConfigItem("PyPI-Conda mapping download directory", "")),
-        ]
-        return [(f"conda.{name}", config) for name, config in _configs]
