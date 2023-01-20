c633645195d0e327ee5cba148ad7d944b0463804
macro128 <29261232+macro128@users.noreply.github.com>
2023-01-20 16:40:43 -0300

    add plugins to pdm_install.py

--- deploy/pdm_install.py
+++ deploy/pdm_install.py
@@ -28,6 +28,10 @@ def get_pdm_executable() -> str:
     return path
 
 
+def parse_list(args):
+    return list(itertools.chain.from_iterable(elem.split(",") for elem in args))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Execute PDM install with some arguments overrides.",
@@ -45,6 +49,13 @@ if __name__ == "__main__":
         help="select groups of optional-dependencies",
     )
     parser.add_argument(
+        "-p",
+        "--plugins",
+        nargs="*",
+        default="",
+        help="list of plugins to install",
+    )
+    parser.add_argument(
         "-",
         dest="args",
         default=[],
@@ -53,7 +64,7 @@ if __name__ == "__main__":
     )
     args = parser.parse_args()
 
-    groups = list(itertools.chain.from_iterable(g.split(",") for g in args.groups))
+    groups = parse_list(args.groups)
     groups = [f"-G {g.strip()}" for g in groups if g.strip()]
 
     cmd_args = args.args + groups
@@ -67,5 +78,9 @@ if __name__ == "__main__":
             cmd_args.append(arg)
 
     executable = args.pdm_executable or get_pdm_executable()
+    plugins = parse_list(args.plugins)
+    for plugin in plugins:
+        subprocess.check_call(["pdm", "self", "add", plugin], executable=executable)
+
     # replace current process with pdm install
     os.execl(executable, "pdm", "install", *cmd_args)
