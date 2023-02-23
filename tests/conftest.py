929aeb1b27789c629957ddd48ee7745ce8b5eab0
macro128 <29261232+macro128@users.noreply.github.com>
2023-02-22 21:58:35 -0300

    tests changes

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,22 +1,109 @@
 """Configuration for the pytest test suite."""
+import os
+import re
 import sys
 from copy import deepcopy
-from tempfile import TemporaryDirectory
 
 import pytest
 import responses
 from pdm.cli.actions import do_init
 from pdm.core import Core
-from pdm.project import Project
+from pdm.project import Config, Project
 
-from pdm_conda import main
+pytest_plugins = "pdm.pytest"
+
+PYTHON_VERSION = sys.version.split(" ")[0]
+REPO_BASE = "https://anaconda.org"
+PYTHON_REQUIREMENTS = [
+    {
+        "name": "lib2",
+        "depends": [],
+        "version": "1.0.0g",
+        "build_number": 0,
+        "url": f"{REPO_BASE}/channel/lib2",
+        "channel": f"{REPO_BASE}/channel",
+        "sha256": "this-is-a-hash",
+        "build_string": "lib2",
+    },
+    {
+        "name": "openssl",
+        "depends": [],
+        "version": "1.1.1s",
+        "build_number": 0,
+        "url": f"{REPO_BASE}/channel/lib2",
+        "channel": f"{REPO_BASE}/channel",
+        "sha256": "this-is-a-hash",
+        "build_string": "lib2",
+    },
+    {
+        "name": "lib",
+        "depends": ["lib2 ==1.0.0g", "openssl >=1.1.1s,<1.1.2a"],
+        "version": "1.0.0",
+        "build_number": 0,
+        "url": f"{REPO_BASE}/channel/lib",
+        "channel": f"{REPO_BASE}/channel",
+        "sha256": "this-is-a-hash",
+        "build_string": "lib",
+    },
+    {
+        "name": "python",
+        "depends": ["lib ==1.0.0"],
+        "build_number": 0,
+        "version": PYTHON_VERSION,
+        "url": f"{REPO_BASE}/channel/python",
+        "channel": f"{REPO_BASE}/channel",
+        "sha256": "this-is-a-hash",
+        "build_string": "python",
+    },
+]
+PYTHON_PACKAGE = PYTHON_REQUIREMENTS[-1]
+CONDA_INFO = [
+    [
+        *PYTHON_REQUIREMENTS,
+        {
+            "name": "another-dep",
+            "depends": [],
+            "version": "1!0.0gg",
+            "build_number": 0,
+            "url": f"{REPO_BASE}/channel/another-dep",
+            "channel": f"{REPO_BASE}/channel",
+            "sha256": "this-is-a-hash",
+            "build_string": "another-dep",
+        },
+        {
+            "name": "another-dep",
+            "depends": [],
+            "build_number": 1,
+            "version": "1!0.0gg",
+            "url": f"{REPO_BASE}/channel/another-dep",
+            "channel": f"{REPO_BASE}/channel",
+            "sha256": "this-is-a-hash",
+            "build_string": "another-dep",
+        },
+        {
+            "name": "dep",
+            "build_number": 0,
+            "depends": ["python >=3.7", "another-dep ==1!0.0gg|==1!0.0g"],
+            "version": "1.0.0",
+            "url": f"{REPO_BASE}/channel/dep",
+            "channel": f"{REPO_BASE}/channel",
+            "sha256": "this-is-a-hash",
+            "build_string": "dep",
+        },
+    ],
+]
+
+CONDA_MAPPING = [{f"{p['name']}-pip": p["name"] for p in CONDA_INFO[0] if p not in PYTHON_REQUIREMENTS}]
 
 
-@pytest.fixture(scope="session")
-def core() -> Core:
-    _core = Core()
-    main(_core)
-    yield _core
+@pytest.fixture(name="core")
+def core_with_plugin(core, monkeypatch) -> Core:
+    from pdm_conda import main
+
+    Config._config_map["python.use_venv"].default = True
+    monkeypatch.setenv("_CONDA_PREFIX", os.getenv("CONDA_PREFIX"))
+    main(core)
+    yield core
 
 
 @pytest.fixture
@@ -25,23 +112,30 @@ def distributions(mocker):
 
 
 @pytest.fixture
-def project(core, distributions) -> Project:
-    with TemporaryDirectory() as tmp_dir:
-        _project = core.create_project(tmp_dir)
-        do_init(
-            _project,
-            name="test",
-            version="0.0.0",
-            python_requires=f"=={PYTHON_VERSION}",
-            author="test",
-            email="test@test.com",
-        )
-        _project.global_config["check_update"] = False
-        yield _project
+def project(core, project_no_init, monkeypatch) -> Project:
+    _project = project_no_init
+    _project.global_config["check_update"] = False
+    _project.global_config["pypi.json_api"] = True
+    _project.global_config["pypi.url"] = REPO_BASE
+    do_init(
+        _project,
+        name="test",
+        version="0.0.0",
+        python_requires=f"=={PYTHON_VERSION}",
+        author="test",
+        email="test@test.com",
+    )
+    monkeypatch.setenv("CONDA_PREFIX", os.getenv("_CONDA_PREFIX"))
+    yield _project
 
 
-@pytest.fixture
-def mock_conda(mocker, conda_response: dict | list, empty_conda_list):
+@pytest.fixture(name="pdm")
+def pdm_run(core, pdm):
+    yield pdm
+
+
+@pytest.fixture(name="conda")
+def mock_conda(mocker, conda_response: dict | list, empty_conda_list: bool = False):
     if isinstance(conda_response, dict):
         conda_response = [conda_response]
     install_response = {
@@ -87,6 +181,57 @@ def mock_conda(mocker, conda_response: dict | list, empty_conda_list):
     yield mocker.patch("pdm_conda.plugin.run_conda", side_effect=_mock)
 
 
+@pytest.fixture(name="pypi")
+def mock_pypi(mocked_responses):
+    def _mocker(conda_response, with_dependencies: list[str] | None = None):
+        if with_dependencies is None:
+            with_dependencies = []
+        for package in conda_response:
+            dependencies = list(package["depends"])
+            requires_python = ""
+            to_delete = []
+            for d in dependencies:
+                if d.startswith("__"):
+                    to_delete.append(d)
+                elif d.startswith("python "):
+                    to_delete.append(d)
+                    if not requires_python:
+                        requires_python = d.split(" ")[-1]
+            for d in to_delete:
+                dependencies.remove(d)
+            name = package["name"]
+            version = package["version"]
+            mocked_responses.get(
+                f"{REPO_BASE}/{name}/",
+                content_type="application/vnd.pypi.simple.v1+json",
+                json=dict(
+                    files=[
+                        {
+                            "url": f"{name}#egg={name}-{re.sub('[a-z]', '', version)}",
+                            "requires-python": requires_python,
+                            "yanked": None,
+                            "dist-info-metadata": False,
+                            "hashes": None,
+                        },
+                    ],
+                ),
+            )
+
+            if name in with_dependencies:
+                mocked_responses.get(
+                    f"{REPO_BASE}/pypi/{name}/{version}/json",
+                    json=dict(
+                        info=dict(
+                            summary="",
+                            requires_python=requires_python,
+                            requires=[d.split("|")[0] for d in dependencies],
+                        ),
+                    ),
+                )
+
+    return _mocker
+
+
 @pytest.fixture
 def mocked_responses():
     with responses.RequestsMock() as rsps:
@@ -99,77 +244,3 @@ def mock_conda_mapping(mocker, mocked_responses, conda_mapping):
     from pdm_conda.mapping import get_pypi_mapping
 
     get_pypi_mapping.cache_clear()
-
-
-PYTHON_VERSION = sys.version.split(" ")[0]
-REPO_BASE = "https://anaconda.org"
-PYTHON_REQUIREMENTS = [
-    {
-        "name": "lib2",
-        "depends": [],
-        "version": "1.0.0g",
-        "build_number": 0,
-        "url": f"{REPO_BASE}/channel/lib2",
-        "channel": f"{REPO_BASE}/channel",
-        "sha256": "this-is-a-hash",
-        "build_string": "lib2",
-    },
-    {
-        "name": "lib",
-        "depends": ["lib2 ==1.0.0g"],
-        "version": "1.0.0",
-        "build_number": 0,
-        "url": f"{REPO_BASE}/channel/lib",
-        "channel": f"{REPO_BASE}/channel",
-        "sha256": "this-is-a-hash",
-        "build_string": "lib",
-    },
-    {
-        "name": "python",
-        "depends": ["lib ==1.0.0"],
-        "build_number": 0,
-        "version": PYTHON_VERSION,
-        "url": f"{REPO_BASE}/channel/python",
-        "channel": f"{REPO_BASE}/channel",
-        "sha256": "this-is-a-hash",
-        "build_string": "python",
-    },
-]
-
-CONDA_INFO = [
-    [
-        *PYTHON_REQUIREMENTS,
-        {
-            "name": "another-dep",
-            "depends": [],
-            "version": "1!0.0gg",
-            "build_number": 0,
-            "url": f"{REPO_BASE}/channel/another-dep",
-            "channel": f"{REPO_BASE}/channel",
-            "sha256": "this-is-a-hash",
-            "build_string": "another-dep",
-        },
-        {
-            "name": "another-dep",
-            "depends": [],
-            "build_number": 1,
-            "version": "1!0.0gg",
-            "url": f"{REPO_BASE}/channel/another-dep",
-            "channel": f"{REPO_BASE}/channel",
-            "sha256": "this-is-a-hash",
-            "build_string": "another-dep",
-        },
-        {
-            "name": "dep",
-            "build_number": 0,
-            "depends": ["python >=3.7", "another-dep ==1!0.0gg|==1!0.0g"],
-            "version": "1.0.0",
-            "url": f"{REPO_BASE}/channel/dep",
-            "channel": f"{REPO_BASE}/channel",
-            "sha256": "this-is-a-hash",
-            "build_string": "dep",
-        },
-    ],
-]
-
-CONDA_MAPPING = [{f"{p['name']}-pip": p["name"] for p in CONDA_INFO[0] if p not in PYTHON_REQUIREMENTS}]
