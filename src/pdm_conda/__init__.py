7f6654e5d61c3162ed289fc0043684cf8c26428c
macro128 <29261232+macro128@users.noreply.github.com>
2023-02-22 21:57:56 -0300

    avoid conda lock

--- src/pdm_conda/__init__.py
+++ src/pdm_conda/__init__.py
@@ -2,12 +2,9 @@ from pdm.core import Core
 
 
 def main(core: Core):
-    from pdm.signals import pre_lock
-
     from pdm_conda.cli.commands.add import Command as AddCommand
     from pdm_conda.cli.commands.remove import Command as RemoveCommand
     from pdm_conda.models.config import CONFIGS
-    from pdm_conda.plugin import lock_conda_dependencies
     from pdm_conda.project import CondaProject
 
     from pdm_conda import utils  # noqa
@@ -18,7 +15,6 @@ def main(core: Core):
     core.register_command(AddCommand)
     core.register_command(RemoveCommand)
 
-    pre_lock.connect(lock_conda_dependencies)
     for name, config in CONFIGS:
         core.add_config(name, config)
 
