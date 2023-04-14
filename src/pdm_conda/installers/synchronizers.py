acd1e8f5150bdfb35edcde7436d358d71dd94e7a
macro128 <29261232+macro128@users.noreply.github.com>
2023-04-14 19:24:20 -0300

    fix

--- src/pdm_conda/installers/synchronizers.py
+++ src/pdm_conda/installers/synchronizers.py
@@ -44,7 +44,9 @@ class CondaSynchronizer(Synchronizer):
         if to_remove:
             to_remove = [p for p in to_remove if p not in self.environment.python_dependencies]
 
-        if self.parallel and any(True for d in to_remove + to_update if isinstance(self.candidates[d], CondaCandidate)):
+        if self.parallel and any(
+            True for d in to_remove + to_update if isinstance(self.candidates.get(d, None), CondaCandidate)
+        ):
             self.environment.project.core.ui.echo("Deactivating parallel uninstall.")
             self.parallel = False
 
