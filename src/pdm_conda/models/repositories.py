27334a7ff69016fb8d264ac8dd22a6e9eff35625
macro128 <29261232+macro128@users.noreply.github.com>
2023-04-19 11:41:57 -0300

    add conda resolution to `CondaRepository`

--- src/pdm_conda/models/repositories.py
+++ src/pdm_conda/models/repositories.py
@@ -1,12 +1,15 @@
+import uuid
+from copy import copy
 from typing import Any, Iterable, Mapping, cast
 
 from pdm._types import Source
 from pdm.models.repositories import BaseRepository, LockedRepository, PyPIRepository
 from pdm.models.requirements import Requirement
 from pdm.models.specifiers import PySpecSet
+from pdm.resolver.python import PythonRequirement
 from unearth import Link
 
-from pdm_conda.conda import conda_search
+from pdm_conda.conda import conda_create, sort_candidates
 from pdm_conda.models.candidates import Candidate, CondaCandidate
 from pdm_conda.models.environment import CondaEnvironment, Environment
 from pdm_conda.models.requirements import (
@@ -20,6 +23,7 @@ class CondaRepository(BaseRepository):
     def __init__(self, sources: list[Source], environment: Environment, ignore_compatibility: bool = True) -> None:
         super().__init__(sources, environment, ignore_compatibility)
         self.environment = cast(CondaEnvironment, environment)
+        self._conda_resolution: dict[str, list[CondaCandidate]] = dict()
 
     def _uses_conda(self, requirement: Requirement) -> bool:
         """
@@ -30,11 +34,26 @@ class CondaRepository(BaseRepository):
         if not isinstance(self.environment, CondaEnvironment):
             return False
         conda_config = self.environment.project.conda_config
-        return isinstance(requirement, CondaRequirement) or (
+        return isinstance(requirement, (CondaRequirement, PythonRequirement)) or (
             isinstance(requirement, NamedRequirement)
             and conda_config.as_default_manager
             and requirement.name not in conda_config.excludes
         )
+
+    def update_conda_resolution(
+        self,
+        requirements: list[Requirement] | None = None,
+        resolution: dict | None = None,
+    ) -> list[CondaRequirement]:
+        """
+        Updates the existing conda resolution if new requirements.
+        :param requirements: list of requirements
+        :param resolution: resolution to override existing
+        :return: list of changed requirements
+        """
+        if resolution is not None:
+            self._conda_resolution = resolution
+        return []
 
     def get_dependencies(self, candidate: Candidate) -> tuple[list[Requirement], PySpecSet, str]:
         if isinstance(candidate, CondaCandidate):
@@ -57,10 +76,58 @@ class CondaRepository(BaseRepository):
 
 
 class PyPICondaRepository(PyPIRepository, CondaRepository):
+    def update_conda_resolution(
+        self,
+        requirements: list[Requirement] | None = None,
+        resolution: dict | None = None,
+    ) -> list[CondaRequirement]:
+        super().update_conda_resolution(requirements, resolution)
+        changed = []
+        requirements = requirements or []
+        requirements = [as_conda_requirement(req) for req in requirements if self._uses_conda(req)]
+        update = False
+        # if any requirement not in saved candidates or incompatible candidates
+        for req in requirements:
+            if update:
+                break
+            key = req.identify()
+            if key not in self._conda_resolution:
+                update = True
+                break
+            for can in self._conda_resolution[key]:
+                if not req.is_compatible(can):
+                    update = True
+                    break
+
+        if update:
+            resolution = conda_create(
+                self.environment.project,
+                requirements,
+                prefix=f"/tmp/{uuid.uuid4()}",
+                dry_run=True,
+            )
+            _requirements = {r.conda_name: r for r in requirements}
+            for name, candidates in resolution.items():
+                req = _requirements.get(name, candidates[0].req)
+                key = req.identify()
+                cans = self._conda_resolution.get(key, [])
+                # add non-existing candidates
+                if any(True for can in candidates if can not in cans):
+                    # add requirement to changed if didn't exist
+                    if key in self._conda_resolution:
+                        changed.append(req)
+                    self._conda_resolution[key] = candidates
+
+        return changed
+
     def _find_candidates(self, requirement: Requirement) -> Iterable[Candidate]:
         if self._uses_conda(requirement):
             requirement = as_conda_requirement(requirement)
-            candidates = conda_search(requirement, self.environment.project)
+            candidates = self._conda_resolution.get(requirement.identify(), [])
+            candidates = [copy(c) for c in candidates if requirement.is_compatible(c)]
+            candidates = list(sort_candidates(self.environment.project, candidates))
+            for can in candidates:
+                can.req = requirement
         else:
             candidates = super()._find_candidates(requirement)
         return candidates
