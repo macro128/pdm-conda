0c86d0606ace5c8627544de57867736bac6caec3
macro128 <macro128@pm.me>
2023-05-16 16:03:37 -0300

    add new commands to init

--- src/pdm_conda/__init__.py
+++ src/pdm_conda/__init__.py
@@ -2,17 +2,20 @@ from pdm.core import Core
 
 
 def main(core: Core):
-    from pdm_conda import utils  # noqa
-    from pdm_conda.cli import utils as cli_utils  # noqa
+    from pdm_conda import utils
+    from pdm_conda.cli import utils as cli_utils
     from pdm_conda.cli.commands.add import Command as AddCommand
+    from pdm_conda.cli.commands.install import Command as InstallCommand
+    from pdm_conda.cli.commands.lock import Command as LockCommand
     from pdm_conda.cli.commands.remove import Command as RemoveCommand
+    from pdm_conda.environments import python
     from pdm_conda.models.config import CONFIGS
     from pdm_conda.project import CondaProject
 
     core.project_class = CondaProject
 
-    core.register_command(AddCommand)
-    core.register_command(RemoveCommand)
+    for cmd in [AddCommand, RemoveCommand, LockCommand, InstallCommand]:
+        core.register_command(cmd)
 
     for name, config in CONFIGS:
         core.add_config(name, config)
